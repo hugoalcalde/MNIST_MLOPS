d60343e12d819b55786e8935b96e449e5a3076d2
Hugo Alcalde <hugoalcaldebenito@gmail.com>
2024-01-09 11:32:53 +0100

    Adding some testing

---
+++ tests/test_data.py
@@ -0,0 +1,20 @@
+from tests import _PATH_DATA
+import torch 
+import pytest 
+
+
+def test_sets_dimmensions() : 
+
+    processed_tensor = torch.load(_PATH_DATA + "processed_tensor.pt")
+
+    train_dataloader = len(processed_tensor["train_loader"].dataset)
+    val_dataloader = len(processed_tensor["val_loader"].dataset)
+    test_dataloader = len(processed_tensor["test_loader"].dataset)
+
+    print(train_dataloader)
+
+    N = 35000
+
+    assert train_dataloader > val_dataloader, "Validation set is bigger than train set"
+    assert train_dataloader + test_dataloader + val_dataloader == N, "There was some data leak in the processing"
+
